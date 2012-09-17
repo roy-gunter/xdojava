ced7f8be01967716ee7b906fba448428d355aa95
roy-gunter <roy.gunter@sage.com>
2012-09-17 09:14:37 -0400

    polishing gem :)

--- lib/xdojava.rb
+++ lib/xdojava.rb
@@ -1,20 +1,20 @@
 module SageoneNaUtils
   class XdoJava
     # Types a character sequence, but without any special chars.
-    def type_keys(type_this)
+    def self.type_keys(type_this)
       #command_line = 'java -classpath ' + get_classpath + ' xdojava ' + type_this
       command_line = 'java xdojava ' + type_this
       system(command_line)
       sleep(0.5)
     end
 
-    def tab
+    def self.tab
       #command_line = 'java -classpath ' + get_classpath + ' xdojava  {TAB}'
       command_line = 'java xdojava  {TAB}'
       system(command_line)
     end
 
-    def tab_back
+    def self.tab_back
       #command_line = 'java -classpath ' + get_classpath + ' xdojava  {SHIFT_TAB}'
       command_line = 'java xdojava  {SHIFT_TAB}'
       system(command_line)
--- xdojava.gemspec
+++ xdojava.gemspec
@@ -1,12 +1,13 @@
 Gem::Specification.new do |s|
   s.name = %q{xdojava}
-  s.version = "1.0.0"
+  s.version = "1.0.1.pre"
   s.date = %q{2012-09-17}
   s.author = "rgunter"
   s.summary = %q{basic cross-platform keystroke emulation}
   s.files = [
     "lib/xdojava.class",
-    "src/xdojava.java"
+    "lib/xdojava.java",
+    "lib/xdojava.rb"
   ]
-  s.require_paths = ["lib", "src"]
+  s.require_paths = ["lib"]
 end
